5896164ec6cc10cb5e7f9988f6d16a7776f1b9e6
Drew Jordan <ajordan12@luc.edu>
2023-06-16 13:05:46 -0500

    updates

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // import required modules and packages
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
